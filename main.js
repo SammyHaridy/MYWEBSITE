6c69c2cd726b037f2683f786ab9427c0c3eb9d5a
SammyHaridy <sammyh11@nycstudents.net>
2022-08-06 20:45:05 -0400

    6th commit

---
+++ main.js
@@ -0,0 +1,141 @@
+const about1 = document.getElementById("test1")
+const aboutA = document.getElementById("testA")
+
+const about2 = document.getElementById("test2")
+const aboutB = document.getElementById("testB")
+
+const about3 = document.getElementById("test3")
+const aboutC = document.getElementById("testC")
+
+const about4 = document.getElementById("test4")
+const aboutD = document.getElementById("testD")
+
+const about5 = document.getElementById("test5")
+const aboutE = document.getElementById("testE")
+
+const about6 = document.getElementById("test6")
+const aboutF = document.getElementById("testF")
+
+const about7 = document.getElementById("test7")
+const aboutG = document.getElementById("testG")
+
+const about8 = document.getElementById("test8")
+const aboutH = document.getElementById("testH")
+
+const about9 = document.getElementById("test9")
+const aboutI = document.getElementById("testI")
+
+const about10 = document.getElementById("test10")
+const aboutJ= document.getElementById("testJ")
+
+
+about1.style.visibility = "hidden"
+about2.style.visibility = "hidden"
+about3.style.visibility = "hidden"
+about4.style.visibility = "hidden"
+about5.style.visibility = "hidden"
+about6.style.visibility = "hidden"
+about7.style.visibility = "hidden"
+about8.style.visibility = "hidden"
+about9.style.visibility = "hidden"
+about10.style.visibility = "hidden"
+
+
+
+
+
+
+
+aboutA.addEventListener('mouseover', function handleMouseOver() {
+    about1.style.visibility = "visible"
+})
+
+aboutA.addEventListener('mouseout', function handleMouseOut() {
+    about1.style.visibility = "hidden"
+})
+
+
+
+aboutB.addEventListener('mouseover', function handleMouseOver() {
+    about2.style.visibility = "visible"
+})
+
+aboutB.addEventListener('mouseout', function handleMouseOut() {
+    about2.style.visibility = "hidden"
+})
+
+
+
+aboutC.addEventListener('mouseover', function handleMouseOver() {
+    about3.style.visibility = "visible"
+})
+
+aboutC.addEventListener('mouseout', function handleMouseOut() {
+    about3.style.visibility = "hidden"
+})
+
+
+
+aboutD.addEventListener('mouseover', function handleMouseOver() {
+    about4.style.visibility = "visible"
+})
+
+aboutD.addEventListener('mouseout', function handleMouseOut() {
+    about4.style.visibility = "hidden"
+})
+
+
+
+aboutE.addEventListener('mouseover', function handleMouseOver() {
+    about5.style.visibility = "visible"
+})
+
+aboutE.addEventListener('mouseout', function handleMouseOut() {
+    about5.style.visibility = "hidden"
+})
+
+
+
+aboutF.addEventListener('mouseover', function handleMouseOver() {
+    about6.style.visibility = "visible"
+})
+
+aboutF.addEventListener('mouseout', function handleMouseOut() {
+    about6.style.visibility = "hidden"
+})
+
+
+
+
+aboutG.addEventListener('mouseover', function handleMouseOver() {
+    about7.style.visibility = "visible"
+})
+
+aboutG.addEventListener('mouseout', function handleMouseOut() {
+    about7.style.visibility = "hidden"
+})
+
+aboutH.addEventListener('mouseover', function handleMouseOver() {
+    about8.style.visibility = "visible"
+})
+
+aboutH.addEventListener('mouseout', function handleMouseOut() {
+    about8.style.visibility = "hidden"
+})
+
+aboutI.addEventListener('mouseover', function handleMouseOver() {
+    about9.style.visibility = "visible"
+})
+
+aboutI.addEventListener('mouseout', function handleMouseOut() {
+    about9.style.visibility = "hidden"
+})
+
+aboutJ.addEventListener('mouseover', function handleMouseOver() {
+    about10.style.visibility = "visible"
+})
+
+aboutJ.addEventListener('mouseout', function handleMouseOut() {
+    about10.style.visibility = "hidden"
+})
+
